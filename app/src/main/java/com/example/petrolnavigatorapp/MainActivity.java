5ea41a59fdc270401debf46143da197e9289070f
Blackwatch98 <danielduczyminski@gmail.com>
2020-08-20 19:51:57 +0200

    open screen and InitialSettingsActivity added

--- app/src/main/java/com/example/petrolnavigatorapp/MainActivity.java
+++ app/src/main/java/com/example/petrolnavigatorapp/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.petrolnavigatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button btn = findViewById(R.id.startButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,InitialSettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
