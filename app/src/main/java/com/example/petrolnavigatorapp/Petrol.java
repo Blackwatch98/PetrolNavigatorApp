3f5b42acbf02e5ddec7004873af43e72a6764cbb
Blackwatch98 <danielduczyminski@gmail.com>
2020-11-03 01:29:18 +0100

    completely new PetrolPopUp layout

--- app/src/main/java/com/example/petrolnavigatorapp/Petrol.java
+++ app/src/main/java/com/example/petrolnavigatorapp/Petrol.java
@@ -10,15 +10,17 @@ import java.util.List;
 public class Petrol implements Serializable {
     private String name;
     private LatLng coordinates;
+    private String address;
     private HashMap<String,Boolean> availableFuels;
     private List<Fuel> fuels;
 
     Petrol(){}
 
-    public Petrol(String name, LatLng coordinates)
+    public Petrol(String name, LatLng coordinates, String address)
     {
         this.name = name;
         this.coordinates = coordinates;
+        this.address = address;
         this.availableFuels = new HashMap<>();
         String[] fuelTypes = {"Benzyna", "Diesel", "LPG", "Etanol", "Elektryczny", "CNG"};
 
@@ -29,8 +31,7 @@ public class Petrol implements Serializable {
 
         String [] names = {"Pb98", "Pb95", "ON", "LPG", "CNG", "Elektryczny", "Etanol"};
         String[] fuelStates = {"fluid", "fluid", "fluid", "gas", "gas","unconv", "unconv"};
-        int [] icons = {R.drawable.benz, R.drawable.benz, R.drawable.diesel,
-                R.drawable.lpg, R.drawable.cng, R.drawable.elektr,  R.drawable.etan};
+        int [] icons = {R.drawable.pb98, R.drawable.pb95,R.drawable.on,R.drawable.lpg2, R.drawable.cng2,R.drawable.ener,R.drawable.e85};
 
         for(int i = 0; i < names.length; i++)
             fuels.add(new Fuel(icons[i],"0.00", names[i], fuelStates[i]));
@@ -50,6 +51,14 @@ public class Petrol implements Serializable {
 
     public void setCoordinates(LatLng coordinates) {
         this.coordinates = coordinates;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public List<Fuel> getFuels() {
