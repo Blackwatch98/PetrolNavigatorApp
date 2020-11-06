5ca628ee0efbc75fcf64d7b3f5cd5becec557a29
Blackwatch98 <danielduczyminski@gmail.com>
2020-11-06 19:39:22 +0100

    new start screen with starting engine effect

--- app/src/main/java/com/example/petrolnavigatorapp/MainActivity.java
+++ app/src/main/java/com/example/petrolnavigatorapp/MainActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +25,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        Button btn = findViewById(R.id.startButton);
+        final MediaPlayer engineSound = MediaPlayer.create(this, R.raw.engine_sound);
+        final Button btn = findViewById(R.id.startButton);
+        text = findViewById(R.id.appTitle);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,InitialSettingsActivity.class);
-                startActivity(intent);
-                finish();
+                final Animation anim = AnimationUtils.loadAnimation(MainActivity.this,R.anim.shake);
+
+                final Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        engineSound.setVolume(1f,1f);
+                        engineSound.start();
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        Intent intent = new Intent(MainActivity.this,InitialSettingsActivity.class);
+                        startActivity(intent);
+                        engineSound.stop();
+                        finish();
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                };
+                anim.setAnimationListener(animationListener);
+                btn.startAnimation(anim);
+                text.startAnimation(anim);
             }
         });
     }
