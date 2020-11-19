6b7ab101230c0b43eba0e78ccca56c0c10d82b1c
Blackwatch98 <danielduczyminski@gmail.com>
2020-11-19 16:58:01 +0100

    simple login activity added

---
+++ app/src/main/java/com/example/petrolnavigatorapp/LoginActivity.java
@@ -0,0 +1,89 @@
+package com.example.petrolnavigatorapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.net.Inet4Address;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText mLogin, mPassword;
+    private Button confirmBtn;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+        mLogin = findViewById(R.id.loginTextEmailAddress);
+        mPassword = findViewById(R.id.loginTextPassword);
+        confirmBtn = findViewById(R.id.loginConfirmBtn);
+        progressBar = findViewById(R.id.progressBar);
+
+        if(mAuth.getCurrentUser() != null)
+        {
+            //sprawdź czy wybrany został przynajmniej promień
+            startActivity(new Intent(getApplicationContext(), InitialSettingsActivity.class));
+            finish();
+        }
+
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String login = mLogin.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(login)) {
+                    mLogin.setError("Login jest wymagany!");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Hasło jest wymagane!");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mPassword.setError("Hasło za krótkie! (min. 6 znaków)");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                mAuth.signInWithEmailAndPassword(login, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            startActivity(new Intent(getApplicationContext(), InitialSettingsActivity.class));
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage().toString(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
